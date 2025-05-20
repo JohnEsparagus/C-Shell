6a14a43655211eb4af8beea3f6988c9475a101af
JohnEsparagus <johnedesparagused@gmail.com>
2025-05-20 11:01:12 +0100

    helper functions isValidCommand

--- main.c
+++ main.c
@@ -19,3 +19,28 @@ const Command commands[] = {
     {"type", "returns brief description of a command", "builtin"}
 };
 
+bool isValidCommand(const char *arg){}
+const char *process_command(const char *arg);
+void print_prompt(void);
+void read_input(char *input, size_t size);
+
+
+int main(int argc, char  *argv[]) {}
+
+bool isValidCommand(const char *arg) {
+  for (int i = 0; i < COMMAND_COUNT; i++) {
+    if (strcmp(arg, commands[i].name)==0) {
+      return true;
+    }
+  }
+  return false;  
+}
+
+const char *process_command(const char *arg) {}
+
+void print_prompt(void) {
+  printf("$ ");
+}
+
+void read_input(char *input, size_t size) {}
+
